68582144612e14737ceee8bc9b97f85c83f69af9
Damien Dart <damiendart@pobox.com>
2021-09-09 20:40:15 +0100

    Replace JavaScript stuff with base PHP stuff.

---
+++ .php-cs-fixer.php
@@ -0,0 +1,19 @@
+<?php
+
+// Copyright (C) 2021 Damien Dart, <damiendart@pobox.com>.
+// This file is distributed under the MIT licence. For more information,
+// please refer to the accompanying "LICENCE" file.
+
+declare(strict_types=1);
+
+use ToolboxSass\Helpers\PHPCSFixerHelper;
+
+return (new PhpCsFixer\Config())
+    ->setRules(PHPCSFixerHelper::getHouseRules())
+    ->setFinder(
+        PhpCsFixer\Finder::create()
+            ->in(__DIR__ . DIRECTORY_SEPARATOR . 'bin')
+            ->in(__DIR__ . DIRECTORY_SEPARATOR . 'src')
+            ->name('*.php')
+            ->name('yassg')
+    );
--- README.md
+++ README.md
@@ -3,4 +3,4 @@ yassg
 
 Yet another static site generator.
 
-You shouldn't use this, especially in its current state.
+A work in progress.
