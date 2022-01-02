c1f7e80cf3844dabc7abf8b42ec671124e6772fc
Damien Dart <damiendart@pobox.com>
2022-01-02 11:50:27 +0000

    Add "FrontMatterService" tests.

---
+++ tests/Unit/Services/FrontMatterServiceTest.php
@@ -0,0 +1,84 @@
+<?php
+
+// Copyright (c) 2022 Damien Dart, <damiendart@pobox.com>.
+// This file is distributed under the MIT licence. For more information,
+// please refer to the accompanying "LICENCE" file.
+
+declare(strict_types=1);
+
+namespace Yassg\Tests\Unit\Services;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Yaml\Exception\ParseException;
+use Symfony\Component\Yaml\Parser;
+use Yassg\Services\FrontMatterService;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class FrontMatterServiceTest extends TestCase
+{
+    public function testParsingADocumentWithValidFrontMatter(): void
+    {
+        $frontMatterService = new FrontMatterService(new Parser());
+
+        [$frontMatter, $content] = $frontMatterService->parseString(
+            "---\nfoo: bar\n---\nLorem ipsum dolor sit amet.",
+        );
+
+        $this->assertEquals(['foo' => 'bar'], $frontMatter);
+        $this->assertEquals('Lorem ipsum dolor sit amet.', $content);
+    }
+
+    public function testParsingADocumentWithCustomFrontMatterDelimeters(): void
+    {
+        $frontMatterService = new FrontMatterService(new Parser());
+        $testStrings = [
+            "{#---\nfoo: bar\n---#}\nLorem ipsum dolor sit amet.",
+        ];
+
+        foreach ($testStrings as $testString) {
+            [$frontMatter, $content] = $frontMatterService->parseString(
+                $testString,
+            );
+
+            $this->assertEquals(['foo' => 'bar'], $frontMatter);
+            $this->assertEquals('Lorem ipsum dolor sit amet.', $content);
+        }
+    }
+
+    public function testParsingADocumentWithInvalidFrontMatter(): void
+    {
+        $frontMatterService = new FrontMatterService(new Parser());
+        $this->expectException(ParseException::class);
+
+        [$frontMatter, $content] = $frontMatterService->parseString(
+            "---\n{foo: bar\n---\nLorem ipsum dolor sit amet.",
+        );
+    }
+
+    public function testParsingADocumentWithJustFrontMatter(): void
+    {
+        $frontMatterService = new FrontMatterService(new Parser());
+
+        [$frontMatter, $content] = $frontMatterService->parseString(
+            "---\nfoo: bar\n---\n",
+        );
+
+        $this->assertEquals(['foo' => 'bar'], $frontMatter);
+        $this->assertEquals('', $content);
+    }
+
+    public function testParsingADocumentWithFrontMatterContainingADelimeter(): void
+    {
+        $frontMatterService = new FrontMatterService(new Parser());
+
+        [$frontMatter, $content] = $frontMatterService->parseString(
+            "---\n---foo: bar\n---\n",
+        );
+
+        $this->assertEquals(['---foo' => 'bar'], $frontMatter);
+        $this->assertEquals('', $content);
+    }
+}
