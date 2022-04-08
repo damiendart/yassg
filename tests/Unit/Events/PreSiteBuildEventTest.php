16d8f5fa16eacbbbaa93c07a760269c3e2c51399
Damien Dart <damiendart@pobox.com>
2022-04-08 23:30:30 +0100

    Add some events tests.

---
+++ tests/Unit/Events/PreSiteBuildEventTest.php
@@ -0,0 +1,29 @@
+<?php
+
+// Copyright (c) 2022 Damien Dart, <damiendart@pobox.com>.
+// This file is distributed under the MIT licence. For more information,
+// please refer to the accompanying "LICENCE" file.
+
+declare(strict_types=1);
+
+namespace Yassg\Tests\Unit\Events;
+
+use PHPUnit\Framework\TestCase;
+use Yassg\Events\PreSiteBuildEvent;
+use Yassg\Files\InputFileCollection;
+
+/**
+ * @covers \Yassg\Events\PreSiteBuildEvent
+ *
+ * @internal
+ */
+class PreSiteBuildEventTest extends TestCase
+{
+    public function testRetrievingCollection(): void
+    {
+        $inputFiles = new InputFileCollection();
+        $event = new PreSiteBuildEvent($inputFiles);
+
+        $this->assertSame($inputFiles, $event->getInputFiles());
+    }
+}
