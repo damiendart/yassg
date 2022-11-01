68603cc24c345ae73f15bae45100569cf00861ae
Damien Dart <damiendart@pobox.com>
2022-11-01 22:08:14 +0000

    Update ".php-cs-fixer.php".

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -8,7 +8,7 @@
 
 declare(strict_types=1);
 
-$header = <<<HEADER
+$header = <<<'HEADER'
 Copyright (c) 2022 Damien Dart, <damiendart@pobox.com>.
 This file is distributed under the MIT licence. For more information,
 please refer to the accompanying "LICENCE" file.
