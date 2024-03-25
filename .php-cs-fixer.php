98ed5f797337c690f56b9ff9a53587afb054f533
Damien Dart <damiendart@pobox.com>
2024-03-25 23:39:00 +0000

    Update copyright-related bumf.

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright (c) 2022 Damien Dart, <damiendart@pobox.com>.
+ * Copyright (C) Damien Dart, <damiendart@pobox.com>.
  * This file is distributed under the MIT licence. For more information,
  * please refer to the accompanying "LICENCE" file.
  */
@@ -9,7 +9,7 @@
 declare(strict_types=1);
 
 $header = <<<'HEADER'
-Copyright (c) 2022 Damien Dart, <damiendart@pobox.com>.
+Copyright (C) Damien Dart, <damiendart@pobox.com>.
 This file is distributed under the MIT licence. For more information,
 please refer to the accompanying "LICENCE" file.
 HEADER;
